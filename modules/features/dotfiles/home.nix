a61dd66231beb013e8b14bdb2c591057604ff465
Muhammad Talha <muhammadtalha.quant@gmail.com>
2026-08-02 20:38:05 +0500

    edit: add self documentation to  home.nix

--- modules/features/dotfiles/home.nix
+++ modules/features/dotfiles/home.nix
@@ -1,13 +1,24 @@
 {
     pkgs,
     userName,
+    inputs,
     ...
 }:
 
 {
 
-    imports = [ ];
+    imports = [
+        (inputs.import-tree ./modules)
+    ];
 
+    # KEEP THIS FILE AS MINIMAL AS YOU CAN AND ADD PER APP CONFIG (<= 200 LOC)
+    # IN A SEPARATE FILE INSIDE THE MODULES DIRECTORY WITH THE NAMING CONVENTION app.nix
+    # AND THEN IT WILL BE AUTO IMPORTED.
+
+    # !=== ** IMPORTANT ** ===!
+    # IF YOUR APPLICATION CONFIG IS BIG, LIKE NEOVIM, THE BEST PRACTICE IS TO CREATE
+    # DIRECTORY FOR THE APPLICATION, AND MODULARIZE IT AND THEN IMPORT THE INITIALIZER NIX
+    # FILE INSIDE THE IMPORTS LIST ABOVE
     programs.home-manager.enable = true;
 
     home = {
